1a6e5b1fc3b9e74a12181a6a94c7b3748a65a651
zeel-codder <zeelprajapati321@gmail.com>
2021-09-15 16:40:08 +0530

    Game Music Done

--- built/index.js
+++ built/index.js
@@ -1,6 +1,7 @@
 var canvas = document.querySelector('#canvas');
-var ctx = canvas.getContext('2d');
 var btn = document.querySelector('#p');
+var shoot = document.querySelector('#Shoot');
+var ctx = canvas.getContext('2d');
 var width = window.innerWidth;
 var height = window.innerHeight;
 canvas.width = width;
@@ -16,22 +17,71 @@ var shot_max = false;
 var enemy_loop = 0;
 var start = false;
 function AddPoint(event) {
+    // console.log(event);
+    // if(event.isTrusted=="Enter") return;
     var index = Math.floor(Math.random() * color.length);
-    var x = event.clientX - p_x;
-    var y = event.clientY - p_y;
+    var x = event.clientX - person.x;
+    var y = event.clientY - person.y;
     var angle = Math.atan2(y, x);
-    var Project = new ProjectDot(p_x, p_y, 5, color[index], { x: Math.cos(angle), y: Math.sin(angle) }, 4);
+    var Project = new ProjectDot(person.x, person.y, 7, color[index], { x: Math.cos(angle), y: Math.sin(angle) }, 4);
     //  console.log(Project);
     if (start) {
+        // shoot.currentTime=0;
+        // shoot.play()
         ProjectPoints.push(Project);
     }
     else {
         start = true;
     }
+    console.log('call1');
+}
+function AddPointUp(event) {
+    // console.log(event);
+    var key = event.key;
+    var nu = 6;
+    console.log(key);
+    if (event.shiftKey || key == "ArrowUp") {
+        var index = Math.floor(Math.random() * color.length);
+        var x = person.x;
+        var y = person.y;
+        var Project = new ProjectDot(person.x, person.y, 5, color[index], { x: 0, y: -1 }, 4);
+        //  console.log(Project);
+        if (start) {
+            if (Math.random() < .3 || key == "ArrowUp") {
+                // shoot.currentTime=0;
+                // shoot.play()
+                ProjectPoints.push(Project);
+            }
+        }
+        else {
+            start = true;
+        }
+    }
+    if (key == "ArrowLeft") {
+        if (person.x - nu >= person.radius) {
+            person.x -= nu;
+        }
+        else {
+            person.x = person.radius;
+        }
+    }
+    else if (key == "ArrowRight") {
+        if (person.x + nu <= width - person.radius) {
+            person.x += nu;
+        }
+        else {
+            person.x = width - person.radius;
+        }
+    }
+    // console.log('call')
 }
 function Start() {
     // for make the gun shot
-    window.addEventListener('click', AddPoint);
+    shoot.loop = true;
+    shoot.play();
+    // window.addEventListener('ArrowLeft', AddPointUp);
+    window.addEventListener('keydown', AddPointUp);
+    window.addEventListener('mousedown', AddPoint);
     //enemy Started
     enemy_loop = setInterval(function () {
         var radius = Math.random() * (30 - 10) + 10;
@@ -41,12 +91,9 @@ function Start() {
         var y;
         x = width * Math.random();
         y = 0 - radius;
-        var x1 = p_x - x;
-        var y2 = p_y - y;
-        var angle = Math.atan2(y2, x1);
         var speed = {
-            y: Math.sin(angle),
-            x: Math.cos(angle)
+            y: 1,
+            x: 0
         };
         var factor = Math.random() < 0.1 ?
             score > 50 ?
@@ -55,12 +102,12 @@ function Start() {
                     2
             :
                 Math.random() < 0.5 ?
-                    3
+                    2
                     :
                         1;
         Enemys.push(new Enemy(x, y, radius, color[index], speed, factor));
         // console.log(Enemys, ProjectPoints)
-    }, 1000);
+    }, 1500);
     // call for gun hoot
     animate();
 }
@@ -86,29 +133,37 @@ function animate() {
     });
     Enemys.forEach(function (e, index1) {
         e.update();
-        ProjectPoints.forEach(function (point, index2) {
-            var diff = Math.hypot(point.x - e.x, point.y - e.y) - point.radius - e.radius;
+        var isOut = e.y - e.radius > height;
+        if (isOut) {
+            setTimeout(function () {
+                Enemys.splice(index1, 1);
+            });
+        }
+        else {
+            ProjectPoints.forEach(function (point, index2) {
+                var diff = Math.hypot(point.x - e.x, point.y - e.y) - point.radius - e.radius;
+                if (diff < 1) {
+                    setTimeout(function () {
+                        score++;
+                        btn.innerHTML = "" + score;
+                        ProjectPoints.splice(index2, 1);
+                        if (e.radius - 10 > 10) {
+                            e.radius -= 10;
+                        }
+                        else {
+                            Enemys.splice(index1, 1);
+                        }
+                    });
+                }
+            });
+            var diff = Math.hypot(person.x - e.x, person.y - e.y) - person.radius - e.radius;
             if (diff < 1) {
                 setTimeout(function () {
-                    score++;
-                    btn.innerHTML = "" + score;
-                    ProjectPoints.splice(index2, 1);
-                    if (e.radius - 10 > 10) {
-                        e.radius -= 10;
-                    }
-                    else {
-                        Enemys.splice(index1, 1);
-                    }
+                    alert("Game End score=" + score);
+                    ReSet();
                 });
+                return;
             }
-        });
-        var diff = Math.hypot(p_x - e.x, p_y - e.y) - 30 - e.radius;
-        if (diff < 1) {
-            setTimeout(function () {
-                alert("Game End score=" + score);
-                ReSet();
-            });
-            return;
         }
     });
 }
@@ -117,9 +172,11 @@ function ReSet() {
     ProjectPoints = [];
     score = 0;
     btn.innerHTML = "" + score;
-    window.removeEventListener('click', AddPoint);
+    window.removeEventListener('mousedown', AddPoint);
+    window.removeEventListener('keydown', AddPointUp);
     if (enemy_loop != 0) {
         clearInterval(enemy_loop);
     }
+    person = new Player(p_x, height, 30, 'blue');
     start = false;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 const canvas: HTMLCanvasElement = document.querySelector('#canvas');
+const btn :HTMLButtonElement = document.querySelector('#p')
+const shoot :HTMLAudioElement =document.querySelector('#Shoot')
+
+
 
 const ctx = canvas.getContext('2d');
-const btn = document.querySelector('#p')
-
-
 
 
 const width: number = window.innerWidth;
@@ -14,86 +15,141 @@ const p_x: number = width / 2;
 const p_y: number = height; let score: number = 0;
 
 
+
 let ProjectPoints: ProjectDot[] = []
-let Enemys: Enemy[] = [];
-const person = new Player(p_x, height, 30, 'blue');
+let Enemys: Enemy[] = [];let person = new Player(p_x, height, 30, 'blue');
 const color: string[] = ['red', 'white', 'yellow', 'green']
 let shot_max: boolean = false;
-let enemy_loop:any=0;
-let start:boolean =false;
+let enemy_loop: any = 0;
+let start: boolean = false;
 
 
 
 
 
 function AddPoint(event: MouseEvent) {
+    // console.log(event);
+    // if(event.isTrusted=="Enter") return;
     const index: number = Math.floor(Math.random() * color.length);
 
-    const x: number = event.clientX - p_x;
-    const y: number = event.clientY - p_y;
+    const x: number = event.clientX - person.x;
+    const y: number = event.clientY - person.y;
     const angle = Math.atan2(y, x);
-    const Project = new ProjectDot(p_x, p_y, 5, color[index],
+    const Project = new ProjectDot(person.x, person.y, 7, color[index],
         { x: Math.cos(angle), y: Math.sin(angle) }, 4)
     //  console.log(Project);
-    if(start){
+    if (start) {
+        // shoot.currentTime=0;
+        // shoot.play()
         ProjectPoints.push(Project);
-    }else{
-        start=true;
+    } else {
+        start = true;
     }
+    console.log('call1')
+}
+
+function AddPointUp(event: KeyboardEvent) {
+
+    // console.log(event);
+    const key: string = event.key;
+
+    const nu:number =6;
+    console.log(key)
+
+    if (event.shiftKey || key=="ArrowUp") {
+
+        const index: number = Math.floor(Math.random() * color.length);
+
+        const x: number = person.x;
+        const y: number = person.y;
+
+        const Project = new ProjectDot(person.x, person.y, 5, color[index],
+            { x: 0, y: -1 }, 4)
+        //  console.log(Project);
+        if (start) {
+            
+            if(Math.random()<.3 || key=="ArrowUp" ){
+                // shoot.currentTime=0;
+                // shoot.play()
+                ProjectPoints.push(Project);
+            }
+        } else {
+            start = true;
+        }
+    } 
+
+
+    if (key == "ArrowLeft") {
+        if (person.x - nu >= person.radius) {
+            person.x -= nu;
+        }else{
+            person.x = person.radius;
+        }
+    } else if (key == "ArrowRight") {
+        if (person.x + nu<= width-person.radius) {
+            person.x += nu;
+        }else{
+            person.x =width- person.radius;
+        }
+    }
+    // console.log('call')
 }
 
 
-function Start() :void{
-// for make the gun shot
-window.addEventListener('click', AddPoint)
+
+function Start(): void {
+    // for make the gun shot
+    shoot.loop=true;
+    shoot.play()
+
+    // window.addEventListener('ArrowLeft', AddPointUp);
+    window.addEventListener('keydown', AddPointUp);
+    window.addEventListener('mousedown', AddPoint)
 
 
-//enemy Started
+    //enemy Started
 
-enemy_loop=setInterval(() => {
+    enemy_loop = setInterval(() => {
 
-    const radius: number = Math.random() * (30 - 10) + 10;
+        const radius: number = Math.random() * (30 - 10) + 10;
 
-    const index: number = Math.floor(Math.random() * color.length);
-    // const color
-
-
-
-    let x: number;
-    let y: number;
+        const index: number = Math.floor(Math.random() * color.length);
+        // const color
 
 
-    x = width * Math.random();
-    y = 0 - radius
+
+        let x: number;
+        let y: number;
 
 
-    const x1: number = p_x - x;
-    const y2: number = p_y - y;
-    const angle = Math.atan2(y2, x1);
-    const speed: velocity = {
+        x = width * Math.random();
+        y = 0 - radius
 
-        y: Math.sin(angle),
-        x: Math.cos(angle)
 
-    }
+        const speed: velocity = {
 
-    const factor:number=Math.random()<0.1?
-                        score>50?
-                        4
-                        : 
-                        2
-                        :
-                        Math.random()<0.5?
-                        3
-                        :
-                        1;
+            y: 1,
+            x: 0
 
-    Enemys.push(new Enemy(x, y, radius, color[index], speed,factor))
+        }
 
-    // console.log(Enemys, ProjectPoints)
-}, 1000)
-// call for gun hoot
-animate()
+        const factor: number = Math.random() < 0.1 ?
+            score > 50 ?
+                4
+                :
+                2
+            :
+            Math.random() < 0.5 ?
+                2
+                :
+                1;
+
+        Enemys.push(new Enemy(x, y, radius, color[index], speed, factor))
+
+        // console.log(Enemys, ProjectPoints)
+    }, 1500)
+    // call for gun hoot
+    animate()
 
 }
 
@@ -103,32 +159,46 @@ function animate(): void {
 
     // console.log(ProjectPoints)
     requestAnimationFrame(animate);
-    ctx.fillStyle="rgba(0,0,0,0.4)"
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "rgba(0,0,0,0.4)"
+ctx.fillRect(0, 0, canvas.width, canvas.height);
     
+
     person.draw();
-    ProjectPoints.forEach((point,index) => {
+    ProjectPoints.forEach((point, index) => {
         point.update();
 
-        const isOut:boolean = point.x+point.radius<0 
-                              || 
-                              point.x-point.radius > width
-                              ||
-                              point.y+point.radius<0
+        const isOut: boolean = point.x + point.radius < 0
+            ||
+            point.x - point.radius > width
+            ||
+            point.y + point.radius < 0
 
-            if (isOut) {
-                setTimeout(() => {
+        if (isOut) {
+            setTimeout(() => {
 
-                   
 
-                    ProjectPoints.splice(index, 1);
-                
-                })
-            }
+
+                ProjectPoints.splice(index, 1);
+
+            })
+        }
     })
 
     Enemys.forEach((e, index1) => {
         e.update()
+
+       
+
+        const isOut: boolean = e.y-e.radius>height;
+
+        if (isOut) {
+            setTimeout(() => {
+                Enemys.splice(index1, 1);
+            })
+          
+            
+        }else{
 
         ProjectPoints.forEach((point, index2) => {
 
@@ -142,42 +212,48 @@ function animate(): void {
 
                     ProjectPoints.splice(index2, 1);
 
-                    if(e.radius-10>10){
-                        e.radius-=10;
-                    }else{
+                    if (e.radius - 10 > 10) {
+                        e.radius -= 10;
+                    } else {
 
                         Enemys.splice(index1, 1);
                     }
                 })
             }
         })
+    
 
-        const diff = Math.hypot(p_x - e.x, p_y - e.y) - 30 - e.radius;
+
+
+        const diff = Math.hypot(person.x - e.x, person.y - e.y) - person.radius - e.radius;
 
         if (diff < 1) {
             setTimeout(() => {
                 alert(`Game End score=${score}`)
                 ReSet();
-            }) 
+            })
             return;
         }
+    }
 
 
     })
 
 }
 
-function ReSet() :void{
-    Enemys=[];
-    ProjectPoints=[];
-    score=0;
+function ReSet(): void {
+    Enemys = [];
+    ProjectPoints = [];
+    score = 0;
     btn.innerHTML = "" + score;
-    window.removeEventListener('click', AddPoint)
-    if(enemy_loop!=0){
+    window.removeEventListener('mousedown', AddPoint)
+    window.removeEventListener('keydown',AddPointUp);
+    if (enemy_loop != 0) {
 
         clearInterval(enemy_loop)
     }
-    start=false;
+    person = new Player(p_x, height, 30, 'blue');
+    start = false;
 }
 
 
